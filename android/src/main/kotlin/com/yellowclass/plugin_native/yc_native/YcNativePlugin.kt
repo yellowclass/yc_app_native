ea1ca0887a636d17e79943192b64b521bb08e525
soninaman <naman@ivypods.com>
2023-09-15 18:21:12 +0530

    network operator

--- android/src/main/kotlin/com/yellowclass/plugin_native/yc_native/YcNativePlugin.kt
+++ android/src/main/kotlin/com/yellowclass/plugin_native/yc_native/YcNativePlugin.kt
@@ -10,6 +10,7 @@ import androidx.annotation.RequiresApi
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.Result
 
 
 /** YcNativePlugin */
